e936746a36a10158361f284c6129341a0c65a576
SonBiell <bg.dasillva@gmail.com>
2026-08-06 03:43:07 -0300

    Adiciona validação confiável em tools-bin/flutter.bat

---
+++ tools-bin/flutter.bat
@@ -0,0 +1,11 @@
+@echo off
+set "APPDATA=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state"
+set "LOCALAPPDATA=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state\local"
+set "PUB_CACHE=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state\pub-cache"
+set "GIT_CONFIG_COUNT=1"
+set "GIT_CONFIG_KEY_0=safe.directory"
+set "GIT_CONFIG_VALUE_0=C:/Users/Thiag/Documents/Codex/2026-08-05/referenced-chatgpt-conversation-this-is-an/tools/flutter"
+set "FLUTTER_NO_VERSION_CHECK=true"
+set "FLUTTER_SUPPRESS_ANALYTICS=true"
+set "FLUTTER_ALREADY_LOCKED=true"
+"C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\tools\flutter\bin\cache\dart-sdk\bin\dart.exe" "C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\tools\flutter\bin\cache\flutter_tools.snapshot" %*
