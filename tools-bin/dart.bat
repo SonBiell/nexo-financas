bca9c5d135e173564eec2841dbda3feb69091499
SonBiell <bg.dasillva@gmail.com>
2026-08-06 03:43:09 -0300

    Adiciona validação confiável em tools-bin/dart.bat

---
+++ tools-bin/dart.bat
@@ -0,0 +1,5 @@
+@echo off
+set "APPDATA=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state"
+set "LOCALAPPDATA=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state\local"
+set "PUB_CACHE=C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\.flutter-state\pub-cache"
+"C:\Users\Thiag\Documents\Codex\2026-08-05\referenced-chatgpt-conversation-this-is-an\tools\flutter\bin\cache\dart-sdk\bin\dart.exe" %*
